0f03d2471ac28072c7b00f46b37549b8faf6c428
Roswer García <roswer13@gmail.com>
2025-08-25 16:35:28 +0200

    refactor: update secure storage interface with additional methods and remove unused auth token provider

--- app_cars_front/lib/core/secure/secure_storage.dart
+++ app_cars_front/lib/core/secure/secure_storage.dart
@@ -1,9 +1,27 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+/// An abstract interface for secure storage operations.
+///
+/// Provides methods to write, read, delete, and clear key-value pairs
+/// in a secure storage mechanism, such as encrypted local storage.
+///
+/// Implementations of this interface should ensure that all data is
+/// stored securely and that sensitive information is protected.
 abstract class ISecureStorageService {
+  /// Writes a [value] associated with the given [key] to secure storage.
+  ///
+  /// If the [key] already exists, its value will be overwritten.
   Future<void> write(String key, String value);
+
+  /// Reads the value associated with the given [key] from secure storage.
+  ///
+  /// Returns the value if it exists, or `null` if the [key] is not found.
   Future<String?> read(String key);
+
+  /// Deletes the value associated with the given [key] from secure storage.
   Future<void> delete(String key);
+
+  /// Deletes all key-value pairs from secure storage.
   Future<void> deleteAll();
 }
 
