138d790724e7edb0247983d0ab565567f980b867
Roswer García <roswer13@gmail.com>
2025-08-25 11:46:06 +0200

    feat: update archictecture 🚧
    - Enhance CI workflows with analysis and coverage checks.
    - Expand feature exports.

--- app_cars_front/lib/features/features.dart
+++ app_cars_front/lib/features/features.dart
@@ -1,1 +1,6 @@
-
+export 'alerts/alerts.dart';
+export 'auth/auth.dart';
+export 'dashboard/dashboard.dart';
+export 'map/map.dart';
+export 'places/places.dart';
+export 'vehicules/vehicules.dart';
