e3db3b489229b80f035859f35160fc57951aa4ed
roswer13 <roswer13@gmail.com>
2025-08-27 17:41:39 +0200

    feat: Update iOS project configuration and dependencies
    - Added Pods project reference to Runner.xcworkspace.
    - Enabled automatic context creation in WorkspaceSettings.
    - Updated Google Maps API key in AppDelegate.
    - Modified Info.plist to use dynamic app name and added location permissions.
    - Included necessary Podfile for iOS dependencies with location permissions.
    - Generated Podfile.lock with updated dependencies.
    - Created new xcscheme files for different build configurations (dev, prod, staging).
    - Added Podfile for macOS with necessary configurations.

--- app_cars_front/ios/Runner/AppDelegate.swift
+++ app_cars_front/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("-") // TODO: Add your API key here.
+    GMSServices.provideAPIKey("AIzaSyATWGy6S42ZD3JE7GPhk4VkRBxZfd0Ze6I") // TODO: Add your API key here.
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
